580f27f14927280e5bcb3b56361b8290d31a0292
timi-petre <gaxxis@yahoo.com>
2022-11-03 12:37:54 +0200

    Login&Register Page included with Passport.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,68 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const router = require('express').Router()
 const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+
+const initializePassport = require('../passport-config')
+router.use(flash())
+router.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    }),
+)
+router.use(passport.initialize())
+router.use(passport.session())
+router.use(methodOverride('_method'))
+
+initializePassport(
+    passport,
+    (email) => users.find((user) => user.email === email),
+    (id) => users.find((user) => user.id === id),
+)
+
 const users = []
-router.get('/', (req, res) => {
-    res.render('index', { title: 'Home Page', name: 'kyle' })
+router.get('/', checkAuthenticated, (req, res) => {
+    res.render('index', { title: 'Home Page', name: req.user.name })
 })
-router.get('/about', (req, res) => {
-    res.render('about', { title: 'About' })
+router.get('/about', checkAuthenticated, (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: req.user.name,
+    })
 })
 
-router.get('/login', (req, res) => {
+router.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs', {
         title: 'Login Page',
         layout: './layouts/sign-layout.ejs',
     })
 })
-router.post('/login', (req, res) => {})
+router.post(
+    '/login',
+    checkNotAuthenticated,
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true,
+    }),
+)
 
-router.get('/register', (req, res) => {
+router.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs', {
         title: 'Register Page',
         layout: './layouts/sign-layout.ejs',
     })
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         users.push({
@@ -36,7 +75,29 @@ router.post('/register', async (req, res) => {
     } catch {
         res.redirect('/register')
     }
-    console.log(users)
 })
+
+router.delete('/logout', (req, res) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err)
+        }
+        res.redirect('/login')
+    })
+})
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 module.exports = router
