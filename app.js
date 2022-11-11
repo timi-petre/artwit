e747bfe2375bd807a64c0caba76184de07577cd6
timi-petre <gaxxis@yahoo.com>
2022-11-11 13:39:25 +0200

    Template literal add to final

--- app.js
+++ app.js
@@ -14,36 +14,25 @@ const port = process.env.PORT
 
 require('./config/passport')(passport)
 
-
 app.use(flash())
 
 const db = require('./config/keys').MongoURI
-
 
 mongoose
     .connect(db, {})
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log(err))
 
-
 app.use(expressLayouts)
 app.set('views', './views')
 app.set('view engine', 'ejs')
 
-
 app.set('layout', './layouts/default')
-
 
 app.use(express.urlencoded({ extended: false }))
 
-
-
 app.use(express.static('public'))
 app.use(express.json())
-
-
-
-
 
 app.use(
     session({
@@ -53,10 +42,8 @@ app.use(
     }),
 )
 
-
 app.use(passport.initialize())
 app.use(passport.session())
-
 
 app.use((req, res, next) => {
     res.locals.success_msg = req.flash('success_msg')
@@ -65,13 +52,11 @@ app.use((req, res, next) => {
     next()
 })
 
-
 app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
 app.use('/create', require('./routes/create'))
 app.use(require('./routes/blog'))
 
-
 app.listen(port, () =>
-    console.log(`Server listening on port http:
+    console.log(`Server listening on port http://localhost:${port} !`),
 )
