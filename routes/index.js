05cf6e9309c4c63b304f7a2979f3ffccdcecc05a
timi-petre <gaxxis@yahoo.com>
2022-11-24 14:24:55 +0200

    Changes made thanks to Eduard

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ const flash = require('connect-flash')
 const passport = require('passport')
 const session = require('express-session')
 
-const User = require('../models/User')
 require('../config/passport')(passport)
 
 router.use(flash())
